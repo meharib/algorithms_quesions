e6ae222542fd87615b483308a0ed2ea694c04d57
Mehari <meharig@gmail.com>
2018-06-20 13:35:52 -0700

    implemented binary search tree

---
+++ src/main/java/trees/Node.java
@@ -0,0 +1,59 @@
+package trees;
+
+/**
+ * Created by Mehari on 6/19/18.
+ */
+public class Node {
+    private int data;
+    public Node left;
+    public Node right;
+    public Node(int data){
+        this.data=data;
+    }
+
+    public void insert(int value){
+        if(value<=data){
+            insertLeft(value);
+        }else{
+            insertRight(value);
+        }
+    }
+
+    private void insertLeft(int value) {
+        if(this.left==null){
+            this.left=new Node(value);
+        }else{
+            this.left.insert(value);
+        }
+    }
+    private void insertRight(int value) {
+        if(this.right==null){
+            this.right=new Node(value);
+        }else{
+            this.right.insert(value);
+        }
+    }
+
+    public boolean contains(int value){
+        if(data==value) return true;
+        if(value<data){
+            if(this.left==null) return false;
+            return this.left.contains(value);
+        }else{
+            if(this.right==null) return false;
+            return this.right.contains(value);
+        }
+    }
+
+    public void printInOrder(){
+        if(this.left!=null){
+            this.left.printInOrder();
+        }
+        System.out.println(data);
+        if(this.right!=null){
+            this.right.printInOrder();
+        }
+    }
+
+
+}
